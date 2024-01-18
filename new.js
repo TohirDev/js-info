2033386e3dc990acddec36a6baa45c36274ea730
TohirDev <isakulovtohir902@gmail.com>
2024-01-18 22:41:09 +0500

    products brougt

--- new.js
+++ new.js
@@ -65,5 +65,56 @@ const newName = {
 function factoryOne(name) {
   return (name = this.name);
 }
+const CARD = document.getElementById("container");
+const CART_BUTTON = document.getElementById("cart_button");
+const CROSS = document.getElementById("cross");
+const COUNT = document.getElementById("count");
+const CART = document.querySelector(".cart");
 
-console.log(factoryOne(newName));
+let cart_store = [];
+// console.log(factoryOne(newName));
+function addProduct(value) {
+  console.log(value);
+}
+fetch("https://dummyjson.com/products", {
+  method: "GET",
+})
+  .then((javob) => {
+    if (javob.ok) {
+      return javob.json();
+    }
+  })
+  .then((data) => {
+    data.products.forEach((value) => {
+      let card = `
+        <div class='card'>
+        <img src="${value.thumbnail}" class="img" />
+        <h1>${value.title}</h1>
+        <p>${value.description}</p>
+        <button class="button">Add to Cart</button>
+        <div>
+        `;
+      CARD.innerHTML += card;
+    });
+    let button = document.querySelectorAll(".button");
+    button.forEach((e, i) => {
+      e.addEventListener("click", function () {
+        cart_store.push(data.products[i]);
+        console.log(Object.values(data.products[i]));
+        console.log(cart_store);
+        COUNT.innerText = cart_store.length;
+      });
+    });
+  });
+
+CART_BUTTON.addEventListener("click", function () {
+  CART.classList.add("hide");
+});
+
+CROSS.addEventListener("click", function () {
+  CART.classList.remove("hide");
+  console.log("hh");
+});
+
+if (cart_store.length >= 1) {
+}
